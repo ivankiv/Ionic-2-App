428b3b0d09b01ea83487520a4e111cfee06953ae
Ivan Ivankiv <ivan.ivankiv27@gmail.com>
2016-12-11 22:05:44 +0200

    Added template for menu page and start game page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import {NavController} from 'ionic-angular';
 
-import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
-  selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navController: NavController) {
 
   }
 
+  startGame(event) {
+    this.navController.push(AboutPage, {});
+  }
 }
